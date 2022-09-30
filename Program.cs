3b2530a8a878b6b17ddfb2210ed95356ae20a2ca
admin <admin@MSI>
2022-09-30 10:15:06 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thune
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
